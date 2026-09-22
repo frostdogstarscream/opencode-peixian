ea3b4e215dc9847465d5e97d34bf13c991ab68e5
frosydogstarscream <838958165@qq.com>
2026-09-22 09:43:04 +0800

    style(peixian): preserve colleague right rail updates for stage2 rollout

--- packages/peixian-console/src/main.tsx
+++ packages/peixian-console/src/main.tsx
@@ -5,4 +5,5 @@ import "./styles.css"
 import "./chat-polish.css"
 import "./visual-polish.css"
 import "./typography.css"
+import "./right-rail.css"
 render(() => <App />, document.getElementById("root")!)
